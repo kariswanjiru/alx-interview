22e37bf31ac5631030c7cf7dc3cecf418fb05df1
kariswanjiru <wanjiruestar@gmail.com>
2022-11-05 12:51:04 +0300

    doc: initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal_triangle(n):
+    x = []
+    if n<=0:
+        print(x)
+    else:
+        for n in x:
+            x+= [1]
+            for i in x:
+                x+=1
+    
+
+pascal_triangle(3)
