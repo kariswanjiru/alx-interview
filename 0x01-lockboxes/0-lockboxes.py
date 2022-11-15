52d65458c24ad0f8c7c7a688ea9f587c6f66728a
kariswanjiru <wanjiruestar@gmail.com>
2022-11-15 13:03:44 +0300

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """Determines if all boxes are openable."""
 
     # Create a set of keys that have not been used yet. This is the starting point for our search.
@@ -19,4 +19,3 @@ def can_unlock_all(boxes):
             unlockables = boxes[key]  # Find all boxes that could be unlocked using this new key (the index is the number). These are stored as values in a dictionary with each value being another dictionary containing all possible ways to get out of any given box using only one specific key (the index is the number). The result will be a list containing lists representing every possible path through the maze where you can get out without having to use more than one specific type of key at once (it's okay if you have multiple types but they must be different). If no such path exists between two given boxes then an empty list will be returned instead (this means that you cannot go from any given box to any other box without getting stuck!). So if you want to know how many ways there are, just count how many paths there are within each nested dictionary! You may also want to check whether or not your code works by running your function with some simple test cases like [[1], [2], [3]]. See below for examples! In addition, note that when looking up values in dictionaries, Python returns None when no value exists rather than raising an exception like most languages do! So even though your function may return False because none of these paths exist between some pair of
         except:
             pass
-        
