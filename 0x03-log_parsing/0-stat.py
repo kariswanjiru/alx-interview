c32178d715316f1ee41dfc3525d45169f1206316
kariswanjiru <wanjiruestar@gmail.com>
2022-11-18 10:52:22 +0300

    initial commit

---
+++ 0x03-log_parsing/0-stat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+The script should read the input from stdin and print the statistics in the following format:
+Total file size: File size: <total size>
+Number of lines by status code: 
+<status code>: <number> (if a status code doesn\’t appear or is not an integer, don\’t print anything for this status code)
+
+
+'''
